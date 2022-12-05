a9e1f02bfe26abde5b703bab07fdc1c94e92e671
Nikolas Caldeira <caldeira.n92@gmail.com>
2022-12-05 00:00:44 -0300

    Repositório tp final sistema web II 6

---
+++ Web/Models/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public float Preco { get; set; }
+        public bool Status { get; set; }
+        public int IdCad { get; set; }
+        public int IdUp { get; set; }
+        public Produto()
+        {
+        }
+        public Produto(string nome, float preco, bool status, int idCad)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+            this.Status = status;
+            this.IdCad = idCad;
+        }
+
+
+
+    }
+
+}
